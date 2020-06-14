8cccb23936fdb291a0dfed2460a091792c3a686b
in88 <ishaan.negi@tesco.com>
2020-06-14 18:57:15 +0530

    base commit

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 home of cloud functions for humit firebase.
 
-- notifications
-
 **login** to firebase using-
 
 ```bash
@@ -34,14 +32,56 @@ firebase init functions
 npm install
 ```
 
+## functions
+
+### messaging notifications
+
+send a notification to the receiver as soon as there is a new message.
+
+#### sample data structure
+
+if a new message is send, a new entry is created at `/conversations/$conversationID/messages/$messageID`.
+
+```
+/humit-dev
+    /conversations:
+        /$conversationID:
+            /last_message: "..."
+            /members: [...]
+            /messages
+                /$messageID
+```
+
+#### trigger rules
+
+The function triggers every time a new message is created at `/conversations/$conversationID/messages/$messageID`.
+
 ## deploy
+
+[FULL DOCS](https://firebase.google.com/docs/functions/manage-functions)
+by default cli looks in `functions/` folder.
+
+to deploy all functions in `fucntions/index.js`:
 
 ```bash
 firebase deploy --only functions
 ```
 
+to deploy a specific function:
+
+```bash
+firebase deploy --only functions:<func-name>
+```
+
+to delete:
+
+```bash
+firebase functions:delete <func-name>
+```
+
 ## external links
 
+- [Based On Sample @](https://github.com/firebase/functions-samples/blob/master/fcm-notifications/functions/index.js)
 - [Getting Started](https://firebase.google.com/docs/functions/get-started)
 - [Config Environment Variables](https://firebase.google.com/docs/functions/config-env)
 - [Structure](https://firebase.google.com/docs/functions/organize-functions)
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,61 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Listens for new messages added to /conversations/:conversation_id/messages/:message_id
+exports.messagingNotifications = functions.database
+    .ref('/conversations/{conversationId}/messages/{messageId}')
+    .onCreate(async (snap, context) => {
+        // Grab the original value of what was written to db
+        const message = snap.val();
+        const conversationId = context.params.conversationId;
+        const messageId = context.params.messageId;
+        console.log('We have a new message ID: ', messageId, 'at conversation ID: ', conversationId);
+
+        // Get userIDs
+        const receiverUID = message.receiver_id;
+        const senderUID = message.sender_id;
+        console.log('Message sent from senderUID: ', senderUID, ' to receiverUID: ', receiverUID);
+
+        // Get device token for receiverUID
+        const getDeviceTokensPromise = admin.database()
+            .ref(`/users/${receiverUID}/notification_id`).once('value');
+        
+        const getSenderPromise = admin.database()
+            .ref(`/users/${senderUID}`).once('value');
+        
+        let tokenSnapshot;
+        let senderSnapshot;
+        const results = await Promise.all([getDeviceTokensPromise, getSenderPromise]);
+        tokenSnapshot = results[0];
+        sender = results[1].val();
+
+        if (!tokenSnapshot.hasChildren()) {
+            return console.log('There are no tokens to send notifications to.');
+        }
+
+        const token = tokenSnapshot.val();
+        if (!token) {
+            return console.log('Notification token is empty.');
+        }
+
+        if (!sender) {
+            return console.log('No sender user found with UID: ', senderUID);
+        }
+
+        // Notification details.
+        const payload = {
+            notification: {
+                title: `${sender.user_handle}`,
+                body: `${sender.user_handle} sent you a message.`,
+                icon: sender.profile_pic_url
+            }
+        };
+
+        // Send notifications to all tokens.
+        // const response = await admin.messaging().sendToDevice(token, payload);
+        // check response errors ?
+        return admin.messaging().sendToDevice(token, payload);
+    });
